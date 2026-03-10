babcc938d7aab40445fb40b8bc18bdb24702a19f
zhangchuyuan <1217647124@qq.com>
2026-03-10 20:08:51 +0800

    feat: 初始化egg项目

---
+++ config/config.default.js
@@ -0,0 +1,35 @@
+/* eslint valid-jsdoc: "off" */
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = (appInfo) => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = (exports = {});
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + "_1773141024826_9849";
+
+  // Configure server port (default is 7001)
+  config.cluster = {
+    listen: {
+      port: 3000,
+    },
+  };
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
